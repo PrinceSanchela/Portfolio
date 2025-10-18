4f8c2ec5768b76528feeca2352d2fa42239b853d
Prince Sanchela <princesanchela66@gmail.com>
2025-10-18 22:25:53 +0530

    adding preloader page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Preloader from "./components/sections/preloader";
+import Preloader from "./components/sections/Preloader";
 import { motion, AnimatePresence } from "framer-motion";
 import Footer from "./components/sections/Footer";
 
